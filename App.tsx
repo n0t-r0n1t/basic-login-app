c202b40983ed2ed4d10f45eb8d6fb14a89d1b867
n0t-r0n1t <ronit.r@blazify.tech>
2023-08-24 12:51:54 +0530

    🐛 fix: redux

--- App.tsx
+++ App.tsx
@@ -4,18 +4,22 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import PostListScreen from './src/PostListScreen';
 import PostDetailsScreen from './src/PostDetailsScreen';
 import Login from './src/Login';
+import {Provider} from 'react-redux';
+import store from './src/store/configureStore';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="PostList" component={PostListScreen} />
-        <Stack.Screen name="PostDetails" component={PostDetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="PostList" component={PostListScreen} />
+          <Stack.Screen name="PostDetails" component={PostDetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
